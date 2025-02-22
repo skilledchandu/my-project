ffb5a0c250095055fe89db5ec20d12a5db2b9900
skilledchandu <mcpl.dvk@gmail.com>
2025-02-22 19:02:29 +0300

    first

---
+++ l.py
@@ -0,0 +1,49 @@
+a = "a"
+
+print(a.upper())
+
+b = "b"
+
+print(b.upper())
+
+c = "c"
+
+print(c.upper())
+
+d = "d"
+
+print(d.upper())
+
+e = "e"
+
+print(e.upper())
+
+f = "f"
+
+print(f.upper())
+
+g = "g"
+
+print(g.upper())
+
+h = "h"
+
+print(h.upper())
+
+i = "i"
+
+print(i.upper())
+
+j = "j"
+
+print(j.upper())
+
+k = "k"
+
+print(k.upper())
+
+l = "l"
+
+print(l.upper())
+
+
